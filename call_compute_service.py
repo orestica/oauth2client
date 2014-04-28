2d984c3949b25b6b0d8726fd843125cbd7b8300d
Orest Bolohan <orest@google.com>
2014-04-28 14:58:36 -0700

    Add testing scripts for GoogleCredential.GetDefaultCredential().

---
+++ call_compute_service.py
@@ -0,0 +1,17 @@
+# To be used to test GoogleCredential.GetDefaultCredential()
+# from local machine and GCE.
+
+from googleapiclient.discovery import build
+from oauth2client.default_credential import GoogleCredential
+
+PROJECT = "bamboo-machine-422"
+ZONE = "us-central1-a"
+
+service = build("compute", "v1",
+    credential=GoogleCredential.GetDefaultCredential())
+
+resource = service.instances()
+request = resource.list(project=PROJECT, zone=ZONE)
+response = request.execute()
+
+print response
---
+++ call_compute_service_from_gae.py
@@ -0,0 +1,25 @@
+# To be used to test GoogleCredential.GetDefaultCredential()
+# from devel GAE (ie, dev_appserver).
+
+import webapp2
+from googleapiclient.discovery import build
+from oauth2client.default_credential import GoogleCredential
+
+PROJECT = "bamboo-machine-422"
+ZONE = "us-central1-a"
+
+def get_instances():
+  service = build("compute", "v1",
+      credential=GoogleCredential.GetDefaultCredential())
+  resource = service.instances()
+  request = resource.list(project=PROJECT, zone=ZONE)
+  return request.execute()
+
+class MainPage(webapp2.RequestHandler):
+
+  def get(self):
+    self.response.write(get_instances())
+
+app = webapp2.WSGIApplication([
+  ('/', MainPage),
+], debug=True)
