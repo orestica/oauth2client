641509d4d930f43bc7f55e7985bb618f32053160
Orest Bolohan <orest@google.com>
2014-06-02 14:10:13 -0700

    Modify the OAuth2Credentials.get_access_token() method to return the expiration information, in addition to the access token.

--- tests/test_gce.py
+++ tests/test_gce.py
@@ -125,8 +125,9 @@ class AssertionCredentialsTests(unittest.TestCase):
     http = httplib2.Http()
     http.request = httplib2_request
 
-    self.assertEquals('this-is-a-token',
-                      credentials.get_access_token(http=http))
+    token = credentials.get_access_token(http=http)
+    self.assertEquals('this-is-a-token', token['access_token'])
+    self.assertIsNone(token['expires_in'])
 
     m.UnsetStubs()
     m.VerifyAll()
